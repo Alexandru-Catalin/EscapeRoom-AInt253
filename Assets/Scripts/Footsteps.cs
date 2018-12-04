787efb40b9d9a2b3f2a1479721950fd56f912898
Alexandru-Catalin <33025234+Alexandru-Catalin@users.noreply.github.com>
2018-12-04 21:45:22 +0000

    Add files via upload
    new portal for heaven.

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+
+    characterController cc;
+    [SerializeField]
+    private AudioSource sound;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        cc = GetComponent<characterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")) 
+        {
+            sound.Play();
+        }
+        else if (!Input.GetButton("Horizontal") && !Input.GetButton("Vertical") && sound.isPlaying)
+        {
+            sound.Stop();
+        }
+
+        if (GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ambientHeaven.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ambientHeaven : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    { 
+        if(other.tag == "Player")
+        {
+            sound.Play();
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            sound.Stop();
+        }
+    }
+}
